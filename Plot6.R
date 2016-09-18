dce5cc34e206ca87a6a1e3b29608b067c108134c
hzgl25 <hzgl25@yahoo.com>
2016-09-17 21:57:18 -0400

    Add files via upload

---
+++ Plot6.R
@@ -0,0 +1,36 @@
+library(plyr)
+library(ggplot2)
+if(!exists("NEI")){
+  NEI <- readRDS("./Week 4/summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("./Week 4/Source_Classification_Code.rds")
+}
+vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+vehiclesSCC <- SCC[vehicles,]$SCC
+vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
+
+# Subset the vehicles NEI data by each city
+vehiclesBaltimoreNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
+vehiclesBaltimoreNEI$city <- "Baltimore City"
+
+vehiclesLANEI <- vehiclesNEI[vehiclesNEI$fips=="06037",]
+vehiclesLANEI$city <- "Los Angeles County"
+
+# Combine the two subsets with city name
+bothNEI <- rbind(vehiclesBaltimoreNEI,vehiclesLANEI)
+
+png("plot6.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(bothNEI, aes(x=factor(year), y=Emissions, fill=city)) +
+  geom_bar(aes(fill=year),stat="identity") +
+  facet_grid(scales="free", space="free", .~city) +
+  guides(fill=FALSE) + theme_bw() +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
+
+print(ggp)
+
+dev.off()
