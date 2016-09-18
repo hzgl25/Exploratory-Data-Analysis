9c4032b480b93586302d12f5b9d12920b81859e0
hzgl25 <hzgl25@yahoo.com>
2016-09-17 21:19:04 -0400

    Add files via upload

--- Plot2.R
+++ Plot2.R
@@ -1,9 +1,8 @@
-## This first line will likely take a few seconds. Be patient!
 if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
+  NEI <- readRDS("./Week 4/summarySCC_PM25.rds")
 }
 if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
+  SCC <- readRDS("./Week 4/Source_Classification_Code.rds")
 }
 
 # Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
@@ -11,8 +10,8 @@ if(!exists("SCC")){
 
 subsetNEI  <- NEI[NEI$fips=="24510", ]
 
-aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+aggreTotByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
 
 png('plot2.png')
-barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+barplot(height=aggreTotByYear$Emissions, names.arg=aggreTotByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
 dev.off()
